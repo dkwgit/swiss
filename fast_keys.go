7d64e254f15111abfcf811093fbe62663d19c21c
David Wright <720482+dkwgit@users.noreply.github.com>
2023-09-29 19:35:26 -0500

    first checkin

---
+++ fast_keys.go
@@ -0,0 +1,26 @@
+package swiss
+
+func (m *Map[K, V]) Keys() []K {
+	var allKeys = make([]K, m.Count())
+	// take a consistent view of the table in case
+	// we rehash during iteration
+	ctrl, groups := m.ctrl, m.groups
+	// pick a random starting group
+	g := randIntN(len(groups))
+	allKeysIndex := 0
+	for n := 0; n < len(groups); n++ {
+		for s, c := range ctrl[g] {
+			if c == empty || c == tombstone {
+				continue
+			}
+			k := groups[g].keys[s]
+			allKeys[allKeysIndex] = k
+			allKeysIndex++
+			g++
+			if g >= uint32(len(groups)) {
+				g = 0
+			}
+		}
+	}
+	return allKeys
+}
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/dolthub/swiss
+module github.com/dkwgit/swiss.git/swiss
 
 go 1.18
 
